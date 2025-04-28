8846f9051ee24818efa21639cf753f86d3710984
Jkutney21 <Jkutney21@gmail.com>
2025-04-28 16:32:49 -0500

    fix

--- vite-react-app/src/pages/StudentFIn.tsx
+++ vite-react-app/src/pages/StudentFIn.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./StudentFIn.css"; // Import the CSS file
+import "./StudentFin.css"; // Import the CSS file
 
 interface FinancialAid {
   id: string;
